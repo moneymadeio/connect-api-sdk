c951796a1d2f53840015dda9b3bd9392ab373473
v_pavelko <shadows0breath@gmail.com>
2021-10-15 16:16:45 +0300

    fix signature generation

--- src/core/signature.ts
+++ src/core/signature.ts
@@ -2,18 +2,18 @@ import * as crypto from 'crypto';
 
 export interface SignatureComponents {
   secretKey: string;
-  apiKey: string;
   body: any;
-  nonce: number;
+  requestUrl: string;
+  timestamp: number;
 }
 
 export const makeSign = (params: SignatureComponents) => {
-  const { apiKey, nonce } = params;
-  const bodyPayloadBuffer = Buffer.from(JSON.stringify(params.body));
-  const hmacBody = `${apiKey}${nonce}${bodyPayloadBuffer}${nonce}${apiKey}`;
-  
+  const { requestUrl, timestamp } = params;
+  const bodyPayload = params.body ? JSON.stringify(params.body) : '';
+  const hmacBody = timestamp + requestUrl + bodyPayload;
+ 
   return crypto
     .createHmac('sha256', params.secretKey)
-    .update(hmacBody)
+    .update(Buffer.from(hmacBody))
     .digest('hex');
 }
