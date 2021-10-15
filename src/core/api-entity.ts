2437d5509c2cc7945d239246632b52555b514ff9
v_pavelko <shadows0breath@gmail.com>
2021-10-15 16:17:40 +0300

    add abstract API class

--- src/core/api-entity.ts
+++ src/core/api-entity.ts
@@ -1,5 +1,21 @@
-import { AxiosInstance } from 'axios';
+import { AxiosInstance, AxiosRequestConfig } from 'axios';
+export abstract class API {
+  protected abstract entityUrl: string; 
+  
+  protected request(req: AxiosRequestConfig) {
+    req.url = `${this.entityUrl}${req.url ? '/' + req.url : '' }`;
 
-export class API {
-  constructor(protected client: AxiosInstance) {}
+    return this
+      .client
+      .request(req)
+      .then(({ data }) => data);
+  }
+
+  constructor(private client: AxiosInstance) {}
+}
+
+export namespace API {
+  export interface PageResponse<T> {
+    data: T[];
+  }
 }
