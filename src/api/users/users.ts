2f9585a90720dabde15742385d3fdbc183b21671
v_pavelko <shadows0breath@gmail.com>
2021-10-19 16:56:09 +0300

    add user missed types/methods

--- src/api/users/users.ts
+++ src/api/users/users.ts
@@ -1,27 +1,36 @@
 import { API } from '../../core/api-entity';
+import { Providers } from '../providers';
 
 export class Users extends API {
   entityUrl = 'users';
 
-  async create(account: Users.CreateUserPayload) {
+  async create(account: Users.CreateUserPayload): Promise<Users.User> {
     return this.request({
       method: 'POST',
       data: account,
     });
   }
 
-  async getOne(userId: string) {
+  async getOne(userId: string): Promise<Users.User> {
     return this.request({ url: userId });
   }
+
+  async addAccount(user: Users.User, account: Users.CreateUserPayload): Promise<Users.Account> {
+    return this.request({
+      url: `${user.id}/accounts`,
+      method: 'POST',
+      data: account,
+    });
+  }
   
-  async removeAccount({ userId, accountId }: Users.AccountQuery) {
+  async removeAccount({ userId, accountId }: Users.AccountQuery): Promise<void> {
     return this.request({
       url: `${userId}/accounts/${accountId}`,
       method: 'DELETE',
     });
   }
 
-  async getAccount({ userId, accountId }: Users.AccountQuery) {
+  async getAccount({ userId, accountId }: Users.AccountQuery): Promise<Users.Account> {
     return this.request({ url: `${userId}/accounts/${accountId}` });
   }
 }
@@ -30,6 +39,27 @@ export namespace Users {
   export interface CreateUserPayload {
     account_id: string;
     token: string;
+  }
+
+  export interface Subaccount {
+    name: string;
+    currency: string;
+    balance: string;
+    type: string;
+    fiat_balance: string;
+    balance_updated_at: string;
+    updated_at: string;
+  }
+
+  export interface Account {
+    id: string;
+    provider: Providers.Provider;
+    subaccounts: Subaccount[];
+  }
+
+  export interface User {
+    id: string;
+    accounts: { id: string; provider: Providers.Provider }[];
   }
 
   export interface AccountQuery {
