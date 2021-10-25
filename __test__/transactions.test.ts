83d3e37a774a2d4b949a606bd5364f1a9b28e08f
v_pavelko <shadows0breath@gmail.com>
2021-10-25 18:04:01 +0300

    fix transactions test

--- __test__/transactions.test.ts
+++ __test__/transactions.test.ts
@@ -2,26 +2,32 @@ import { sdk, testAccountId, testUserId } from './env';
 
 describe('transactions API', () => {
   describe('getList', () => {
-    it('should return data', async () => {
+    it('should return pagintated data', async () => {
       const trs = await sdk.transactions.getList({
         userId: testUserId,
         accountId: testAccountId,
       });
 
       expect(trs).toEqual(
-        expect.arrayContaining([
-          expect.objectContaining({
-            account_id: expect.any(String),
-            amount: expect.any(Number),
-            iso_currency_code: expect.any(String),
-            categories: [],
-            status: expect.any(String),
-            transaction_type: expect.any(String),
-            subaccount_id: expect.any(String),
-            issued_at: expect.any(String),
-            transaction_id: expect.any(String),
+        expect.objectContaining({
+          pagination: expect.objectContaining({
+            limit: expect.any(Number),
+            offset: expect.any(Number),
           }),
-        ]),
+          data: expect.arrayContaining([
+            expect.objectContaining({
+              account_id: expect.any(String),
+              amount: expect.any(Number),
+              iso_currency_code: expect.any(String),
+              categories: [],
+              status: expect.any(String),
+              transaction_type: expect.any(String),
+              subaccount_id: expect.any(String),
+              issued_at: expect.any(String),
+              transaction_id: expect.any(String),
+            }),
+          ]),
+        })
       );
     });
   });
