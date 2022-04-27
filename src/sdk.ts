01663f032b46eecd0f7aaeb9513b523aa4038fcf
Meylis <meylis.atamuradov@gmail.com>
2022-04-27 19:32:30 +0500

    prittier deleted

--- src/sdk.ts
+++ src/sdk.ts
@@ -4,9 +4,7 @@ import * as api from "./api";
 import { makeSign } from "./core/signature";
 
 const { DEVELOPMENT_URL } = process.env;
-export const PROJECT_API_URL =
-  DEVELOPMENT_URL ||
-  "https://project-api-dot-moneymade-connect.uc.r.appspot.com";
+export const PROJECT_API_URL = DEVELOPMENT_URL || 'https://project-api-dot-moneymade-connect.uc.r.appspot.com';
 
 export class MoneymadeSDK {
   private axios: AxiosInstance;
@@ -21,9 +19,10 @@ export class MoneymadeSDK {
 
   async getProjectUrls() {
     const { apiKey } = this.config;
-    const { data } = await Axios.get(`${PROJECT_API_URL}/api/v1/environment`, {
-      headers: { ["api-key"]: apiKey },
-    });
+    const { data } = await Axios.get(
+      `${PROJECT_API_URL}/api/v1/environment`,
+      { headers: { ['api-key']: apiKey } },
+    );
 
     return {
       base_api_url: data.account_api_url,
@@ -31,35 +30,37 @@ export class MoneymadeSDK {
     };
   }
 
-  async init(): Promise<MoneymadeSDK> {    
+  async init(): Promise<MoneymadeSDK> {
     const { base_api_url } = await this.getProjectUrls();
 
     this.axios = Axios.create({
-      baseURL: `${base_api_url}/api/${this.config.apiVersion || "v1"}`,
+      baseURL: `${base_api_url}/api/${this.config.apiVersion || 'v1'}`,
       headers: {
-        ["x-mm-api-key"]: this.config.apiKey,
-        ["content-type"]: "application/json",
+        ['x-mm-api-key']: this.config.apiKey,
+        ['content-type']: 'application/json',
       },
     });
 
-    this.axios.interceptors.request.use((request) => {
-      const timestamp = Date.now();
+    this.axios.interceptors.request.use(
+      (request) => {
+        const timestamp = Date.now();
 
-      if (request.url.endsWith("users") || request.url.endsWith("sessions")) {
-        request.baseURL = request.baseURL.replace("v1", "v2");
-      }
+        if(request.url.endsWith('users') || request.url.endsWith('sessions')) {
+          request.baseURL = request.baseURL.replace('v1', 'v2');
+        }
 
-      request.headers["x-mm-api-key"] = this.config.apiKey;
-      request.headers["x-mm-request-timestamp"] = timestamp;
-      request.headers["x-mm-request-signature"] = makeSign({
-        secretKey: this.config.secret,
-        timestamp,
-        requestUrl: `${request.baseURL}/${request.url}`,
-        body: request.data ? request.data : "",
-      });
+        request.headers['x-mm-api-key'] = this.config.apiKey;
+        request.headers['x-mm-request-timestamp'] = timestamp;
+        request.headers['x-mm-request-signature'] = makeSign({
+          secretKey: this.config.secret,
+          timestamp,
+          requestUrl: `${request.baseURL}/${request.url}`,
+          body: request.data ? request.data : '',
+        });
 
-      return request;
-    });
+        return request;
+      },
+    );
 
     this.currencies = new api.Currencies(this.axios);
     this.users = new api.Users(this.axios);
